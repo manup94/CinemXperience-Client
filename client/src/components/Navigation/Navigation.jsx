b2e97c2d3b16d8d621a7b210e5cf46ba33dec530
Ignacio Gonzalez-Bueno <igbescobar@gmail.com>
2023-05-29 16:33:26 +0200

    Fix navbar

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -6,6 +6,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom'
 import { AuthContext } from './../../context/auth.context'
 import { ThemeContext } from '../../context/theme.context';
+import './Navigation.css'
 
 const Navigation = () => {
 
@@ -13,11 +14,18 @@ const Navigation = () => {
 
     const { theme, switchTheme } = useContext(ThemeContext)
 
+    const themeStyle = {
+        backgroundColor: theme === 'dark' ? 'rgb(248 249 250)' : '#212429',
+        color: theme === 'dark' ? 'black' : 'white',
+    }
+
     const variant = theme === 'light' ? 'dark' : 'light'
 
 
 
     return (
+
+
         <Navbar bg={variant} variant={variant} expand="lg" className='mb-5'>
             <Container>
                 <Navbar.Brand>
@@ -28,13 +36,9 @@ const Navigation = () => {
                     <Nav className="me-auto">
                         <Link to={'/pass'} className='nav-link'> Sesiones </Link>
                         <Link to={'/combos'} className='nav-link'> Combos </Link>
-                        <NavDropdown title="Admin" id="basic-nav-dropdown">
-                            <NavDropdown.Item>
-                                <Link to={'/admin/pass/create'} className='nav-link'> Nueva Sesión </Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item>
-                                <Link to={'/admin/combo/create'} className='nav-link'> Nuevo Combo </Link>
-                            </NavDropdown.Item>
+                        <NavDropdown title="Admin" id="basic-nav-dropdown" className="custom-nav-dropdown">
+                            <Link to={'/admin/pass/create'} className='dropdown-item'> Nueva Sesión </Link>
+                            <Link to={'/admin/combo/create'} className='dropdown-item'> Nuevo Combo </Link>
                         </NavDropdown>
                         {
                             user
@@ -43,17 +47,13 @@ const Navigation = () => {
                                     <NavDropdown.Item>
                                         <Link onClick={logout} className='nav-link'> Logout </Link>
                                     </NavDropdown.Item>
-                                    <Link to={'/profile'} className='nav-link'> hello there, {user.username}! </Link>
+                                    <Link to={'/profile'} className='nav-link'> {user.username}! </Link>
                                 </>
                                 :
                                 <>
-                                    <NavDropdown title="Auth" id="basic-nav-dropdown">
-                                        <NavDropdown.Item>
-                                            <Link to={'/signup'} className='nav-link'> Registro </Link>
-                                        </NavDropdown.Item>
-                                        <NavDropdown.Item >
-                                            <Link to={'/login'} className='nav-link'> Iniciar Sesion </Link>
-                                        </NavDropdown.Item>
+                                    <NavDropdown title="Auth" id="basic-nav-dropdown" className="custom-nav-dropdown">
+                                        <Link to={'/signup'} className='dropdown-item'> Registro </Link>
+                                        <Link to={'/login'} className='dropdown-item'> Iniciar Sesion </Link>
                                     </NavDropdown>
                                 </>
                         }
